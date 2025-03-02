59a3738ac1d36c43fd2e2930f39a0779c447f517
amir <amir.temirbulatov@narxoz.kz>
2025-03-02 15:30:28 +0500

    Configure Spring with Project Config

--- src/main/java/org/example/config/ProjectConfig.java
+++ src/main/java/org/example/config/ProjectConfig.java
@@ -1,4 +1,11 @@
 package org.example.config;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
+
 }
