95f91ab41f331f66005acb3d774c74905e860ef4
amir <amir.temirbulatov@narxoz.kz>
2025-03-02 15:34:06 +0500

    Add LazyBean and EagerBean

---
+++ src/main/java/org/example/singletonBeans/EagerBean.java
@@ -0,0 +1,7 @@
+package org.example.singletonBeans;
+
+public class EagerBean {
+    public EagerBean(){
+        System.out.println("Eager bean is created!");
+    }
+}
---
+++ src/main/java/org/example/singletonBeans/LazyBean.java
@@ -0,0 +1,7 @@
+package org.example.singletonBeans;
+
+public class LazyBean {
+    public LazyBean(){
+        System.out.println("Lazy bean is created!");
+    }
+}
