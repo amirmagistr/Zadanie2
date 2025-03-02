e73858126b4d279eba3f06ab346f2bef28584991
amir <amir.temirbulatov@narxoz.kz>
2025-03-02 15:35:52 +0500

    Implementation of Lazy and Eager beans in Config class

--- src/main/java/org/example/config/ProjectConfig.java
+++ src/main/java/org/example/config/ProjectConfig.java
@@ -1,11 +1,23 @@
 package org.example.config;
 
 
+import org.example.singletonBeans.EagerBean;
+import org.example.singletonBeans.LazyBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 @ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
-
+    @Bean
+    @Lazy
+    public LazyBean lazyBean(){
+        return new LazyBean();
+    }
+    @Bean
+    public EagerBean eagerBean(){
+        return new EagerBean();
+    }
 }
